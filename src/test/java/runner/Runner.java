5222198a7ef9d8d746bcc47fd67def16d612c116
AnanthiShankar <116884738+AnanthiShankar@users.noreply.github.com>
2023-01-16 15:16:39 +0530

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,20 +1,29 @@
 package runner;
 
 
+
+import org.testng.annotations.AfterClass;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import utility.BrowserUtil;
+import utility.FirstandFinal;
 
 
 @CucumberOptions(
 	
 	features=("src/test/resources/Features"),
 	glue={"StepDefinitions"},
-	plugin = {"pretty", "json:target/json_report.json"},
+	plugin = {"pretty","summary", "json:target/json_report.json", "html:target/cucumber-report.html"},
 	monochrome = true,
 	tags= ("@Regression"))
 	
 public class Runner extends AbstractTestNGCucumberTests{
 
-}
-//target plugins like monochrome ,pretty
-//add tags and reporting in runner file
+
+
+
+@AfterClass
+public void tearDown() {
+	BrowserUtil.exitBrowser(FirstandFinal.driver);
+}	
