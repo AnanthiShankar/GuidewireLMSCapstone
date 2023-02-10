67d92ff9ae026505e171d0da75ce9c9b6cd16e40
AnanthiShankar <116884738+AnanthiShankar@users.noreply.github.com>
2023-02-10 11:02:49 +0530

    Update BrowserUtil.java
    browserutil changes

--- src/main/java/utility/BrowserUtil.java
+++ src/main/java/utility/BrowserUtil.java
@@ -7,55 +7,66 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.firefox.FirefoxBinary;
 
 public class BrowserUtil {
-	//from selenium import webdriver
-	//from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-	
-
-	/*
-	 * File pathToBinary = new
-	 * File("C:\\Program Files\\Mozilla Firefox\\firefox.exe"); FirefoxBinary
-	 * ffBinary = new FirefoxBinary(pathToBinary); FirefoxProfile firefoxProfile =
-	 * new FirefoxProfile(); FirefoxDriver _driver = new
-	 * FirefoxDriver(ffBinary,firefoxProfile);
-	 */
 	
 	
-		public static WebDriver initApplication(WebDriver driver,String browserName,String appUrl) throws InterruptedException {
-			if (browserName.equalsIgnoreCase("Chrome")){
-				
-				System.setProperty("webdriver.chrome.driver","src/main/resources/drivers/chromedriver.exe");
-				driver=new ChromeDriver();
-			}else if (browserName.equalsIgnoreCase("Edge")) {
-				System.setProperty("webdriver.edge.driver","src/main/resources/drivers/msedgedriver.exe");
-				driver=new EdgeDriver();
-			}else if (browserName.equalsIgnoreCase("Firefox")){
-				
-						
-				System.setProperty("webdriver.gecko.driver","src/main/resources/drivers/geckodriver.exe");
-				driver=new FirefoxDriver();
-			}
-			else {
-				System.out.println("This driver is not supported at this moment");
-			}
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
 			
-			driver.manage().window().maximize();
+		public static void launchBrowser(WebDriver driver,String appUrl) {			
+			//driver.get(appUrl);	
+			System.out.println(appUrl);
 			driver.get(appUrl);
-			return driver;
-			
 		}
 		
 		public static  void exitBrowser(WebDriver driver) {
 			driver.quit();
 			
+		}
+
+		public static WebDriver driverManager(String browserName) {
+			WebDriver driver = null;
+			if (browserName.equalsIgnoreCase("Chrome")){
+				
+				System.setProperty("webdriver.chrome.driver","src/main/resources/drivers/chromedriver.exe");
+				ChromeOptions options = new ChromeOptions();
+				//WebDriver driver = new ChromeDriver();
+				 options.addArguments("--headless");
+				 driver = new ChromeDriver(options);
+				
+				
+			}else if (browserName.equalsIgnoreCase("Edge")) {
+				System.setProperty("webdriver.edge.driver","src/main/resources/drivers/msedgedriver.exe");
+				EdgeOptions options = new EdgeOptions();
+				//WebDriver driver = new ChromeDriver();
+				 options.addArguments("--headless");
+				driver=new EdgeDriver(options);
+			}else if (browserName.equalsIgnoreCase("Firefox")){				
+						
+				System.setProperty("webdriver.gecko.driver","src/main/resources/drivers/geckodriver.exe");
+				FirefoxOptions options = new FirefoxOptions();
+				//WebDriver driver = new ChromeDriver();
+				 options.addArguments("--headless");
+				driver=new FirefoxDriver(options);
+			}
+			else if (browserName.equalsIgnoreCase("IE")){				
+			
+			System.setProperty("webdriver.ie.driver","src/main/resources/drivers/iediagcmd.exe");
+			driver=new InternetExplorerDriver();
+			}
+			else {
+				System.out.println("This driver is not supported at this moment");
+			}
+						return driver;
 		}
 		
 		
